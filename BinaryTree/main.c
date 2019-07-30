2d8c11893a586efbe592be8cc6db59da03301d91
Adam-Xi <321041835@qq.com>
2019-07-30 22:36:00 +0800

    二叉树的基本接口

--- BinaryTree/main.c
+++ BinaryTree/main.c
@@ -6,26 +6,26 @@ int main()
 {
 	BTNode * testTree = BinaryTreeCreate("ABD##E#H##CF##G##");
 
-	/*
-	BinaryTreePrevOrder(testTree);
-	putchar('\n');
-	BinaryTreeInOrder(testTree);
-	putchar('\n');
-	BinaryTreePostOrder(testTree);
-	putchar('\n');
+	
+//	BinaryTreePrevOrder(testTree);
+//	putchar('\n');
+//	BinaryTreeInOrder(testTree);
+//	putchar('\n');
+//	BinaryTreePostOrder(testTree);
+//	putchar('\n');
+//
+//	BinaryTreeLevelOrder(testTree);
+//	putchar('\n');
+//
+//	printf("%d\n", BinaryTreeSize(testTree));
+//	printf("%d\n", BinaryTreeLeafSize(testTree));
+	
 
-	BinaryTreeLevelOrder(testTree);
-	putchar('\n');
-
-	printf("%d\n", BinaryTreeSize(testTree));
-	printf("%d\n", BinaryTreeLeafSize(testTree));
-	*/
 //	BinaryTreePrevOrderNonR(testTree);
 	putchar('\n');
 	BinaryTreeInOrderNonR(testTree);
 	putchar('\n');
 	BinaryTreePostOrderNonR(testTree);
-
 
 	system("pause");
 	return 0;
